2a5dea574dc148f22c3bb6cf4cec20ac3ade2d75
Shashikant Yadav <sashikant12rao@gmail.com>
2024-11-04 15:29:34 +0530

    Changed case of import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from './context/authContext';
 import ProtectedRoute from './components/ProtectedRoute';
 
 import Login from './pages/Login';
